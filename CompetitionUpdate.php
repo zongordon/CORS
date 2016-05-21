58465850fe758f90b7f4a76d61692db0972443dd
e120130 <e120130@SEESKW10259405.vcn.ds.volvo.net>
2016-05-21 20:17:32 +0200

    v3.0.4 - enhancement (CompetitionsList.php, CompetitionInsert.php, CompetitionUpdate.php): - Added function to change current competition direclty in the list - Added function to update and insert new competition setting only one current competition (active) at a time - Removed mb_convert_case from Competition Name

--- CompetitionUpdate.php
+++ CompetitionUpdate.php
@@ -1,5 +1,6 @@
 <?php
-//Added maximum allowed number of registrations before registration will close
+//Added function to only have one current competition (active) at a time
+//Removed mb_convert_case from Competition Name
 
 ob_start();
 
@@ -41,8 +42,8 @@ $totalRows_rsCompetition = mysql_num_rows($rsCompetition);
         <div class="error">
 <?php
  //Validate the form if button is clicked        
- if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "CompForm")) {
-    $comp_name = encodeToISO(mb_convert_case($_POST['comp_name'], MB_CASE_TITLE,"ISO-8859-1"));
+if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "CompForm")) {
+    $comp_name = encodeToISO($_POST['comp_name']);
     $comp_start_date = $_POST['comp_start_date'];
     $comp_end_date = $_POST['comp_end_date'];
     $comp_end_reg_date = $_POST['comp_end_reg_date'];
@@ -97,11 +98,11 @@ $totalRows_rsCompetition = mysql_num_rows($rsCompetition);
     }	                    
 } 
 
-  else {
+else {
     $output_form = 'yes';
-  	}
+}
 
-  	if ($output_form == 'yes') {
+if ($output_form == 'yes') {
 ?>          
         </div>
 <h3>&Auml;ndra &ouml;nskade v&auml;rden och klicka p&aring; &quot;Spara&quot; f&ouml;r att spara och g&aring; tillbaka till listan &ouml;ver t&auml;vlingar.  </h3>
@@ -153,13 +154,21 @@ $totalRows_rsCompetition = mysql_num_rows($rsCompetition);
     </form>
     <p>&nbsp;</p>
 <?php    
-  	} 
+} 
 //Save the competition information
 else if ($output_form == 'no') {
 
         //If button is clicked for updating then update to columns from data in the form
         if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "CompForm")) {
-        $updateSQL = sprintf("UPDATE competition SET comp_name=%s, comp_start_date=%s, comp_end_date=%s, comp_end_reg_date=%s, comp_max_regs=%s , comp_current=%s WHERE comp_id=%s",
+                // Set all competitions first to non-current (0) if the new competition shall be current
+//            echo "Data: ".$_POST["comp_current"];
+                if ($_POST["comp_current"] == "on") {
+                $resetSQL = sprintf("UPDATE competition SET comp_current = 0");
+                mysql_select_db($database_DBconnection, $DBconnection);
+                $Result1 = mysql_query($resetSQL, $DBconnection) or die(mysql_error());
+                }
+        // Update all competition data          
+        $updateSQL = sprintf("UPDATE competition SET comp_name=%s, comp_start_date=%s, comp_end_date=%s, comp_end_reg_date=%s, comp_max_regs=%s, comp_current=%s WHERE comp_id=%s",
                        GetSQLValueString($comp_name, "text"),
                        GetSQLValueString($_POST['comp_start_date'], "date"),
                        GetSQLValueString($_POST['comp_end_date'], "date"),
@@ -168,15 +177,15 @@ else if ($output_form == 'no') {
                        GetSQLValueString(isset($_POST['comp_current']) ? "true" : "", "defined","1","0"),
                        GetSQLValueString($_POST['comp_id'], "int"));
 
-        mysql_select_db($database_DBconnection, $DBconnection);
-        $Result1 = mysql_query($updateSQL, $DBconnection) or die(mysql_error());
-    //After update redirect to page listing the competitions
-    $updateGoTo = "CompetitionList.php?" . $row_rsCompetition['comp_id'] . "=" . $row_rsCompetition['comp_id'] . "";
-    if (isset($_SERVER['QUERY_STRING'])) {
-    $updateGoTo .= (strpos($updateGoTo, '?')) ? "&" : "?";
-    $updateGoTo .= $_SERVER['QUERY_STRING'];
-    }
-    header(sprintf("Location: %s", $updateGoTo));
+            mysql_select_db($database_DBconnection, $DBconnection);
+        $Result2 = mysql_query($updateSQL, $DBconnection) or die(mysql_error());
+            //After update redirect to page listing the competitions
+            $updateGoTo = "CompetitionList.php?" . $row_rsCompetition['comp_id'] . "=" . $row_rsCompetition['comp_id'] . "";
+            if (isset($_SERVER['QUERY_STRING'])) {
+            $updateGoTo .= (strpos($updateGoTo, '?')) ? "&" : "?";
+            $updateGoTo .= $_SERVER['QUERY_STRING'];
+            }
+        header(sprintf("Location: %s", $updateGoTo));
         }
 }
 ?>
