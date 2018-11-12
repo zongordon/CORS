2ba0875b3c1cfe42e3dddafc817818c0ee373a75
e120130 <e120130@SEESKW10259405.vcn.ds.volvo.net>
2018-11-12 18:42:19 +0100

    v3.1.9 Made PDF file rendering code site independent. Added Draws.php to show club specific classes with registrations and links to elimination ladders. Bug fix: - Added closing cursor ($stmt_rsCurrentComp) and DB connection as that is included in header.php: footer.php - Added kill statement: ClassDelete.php, ClassUpdate.php Enhancements: - Added code to make it site independent: DomPDF.php - New page Draws.php to handle viewing of elimination ladders either html or pdf - Removed function and links to elimination ladders as that are replaced by Draws.php: ClassesList_loggedout.php, ClassesList.php - Removed kill DB as it's included in footer.php: all pages with DB connection - Added link to Draws.php to appear when the competition as raffled: news_sponsors_nav.php Information change: - Changed title from "Lottning": navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,11 +1,11 @@
 <?php
-//Added link to Rep_Summary_Admin.php
+//Changed title from "Lottning"
 
 global $_SESSION;
 //Logged in Super Admin
 if (isset($_SESSION['MM_AccountId']) && isset($_SESSION['MM_Level']) && $_SESSION['MM_Level'] == "1") { 
 ?>
-<a href="AccountsList.php">Konton</a>|<a href="AccountInsert.php">Nytt konto</a>|<a href="MessagesHandle.php">Hantera nyheter</a>|<a href="CompetitionList.php">T&auml;vlingar</a>|<a href="CompetitionInsert.php">Ny t&auml;vling</a>|<a href="ClassInsert.php">Ny t&auml;vlingsklass</a>|<a href="RegsHandleAll.php">Hantera anm&auml;lningar</a>|<a href="Raffle.php">Lottning</a>|<a href="LogsList.php">Loggar</a>|<br><a href="Rep_Summary.php">Publika rapporter</a>|<a href="Rep_Summary_Admin.php">Admin-rapporter</a>|<a href="Logout.php">Logga ut</a>
+<a href="AccountsList.php">Konton</a>|<a href="AccountInsert.php">Nytt konto</a>|<a href="MessagesHandle.php">Hantera nyheter</a>|<a href="CompetitionList.php">T&auml;vlingar</a>|<a href="CompetitionInsert.php">Ny t&auml;vling</a>|<a href="ClassInsert.php">Ny t&auml;vlingsklass</a>|<a href="RegsHandleAll.php">Hantera anm&auml;lningar</a>|<a href="Raffle.php">Hantera lottning</a>|<a href="LogsList.php">Loggar</a>|<br><a href="Rep_Summary.php">Publika rapporter</a>|<a href="Rep_Summary_Admin.php">Admin-rapporter</a>|<a href="Logout.php">Logga ut</a>
 <?php 
 }
 //Logged in registered user
